90974e6a3abc334aaef6255c586f2d87c7087e1b
Soufiane <soufianenaji919@gmail.com>
2026-08-10 20:44:38 +0100

    feat: implement authentication and settings page

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,5 +1,32 @@
+import { useContext } from "react";
+import { AuthContext } from "../context/AuthContext";
+
 const Settings = () => {
-  return <div>Settings</div>;
+  const { user, logout } = useContext(AuthContext);
+
+  return (
+    <div className="settings-page">
+      <div className="setting-header">
+        <div>
+          <h1>Settings</h1>
+          <p>Account Information</p>
+        </div>
+        <button className="btn-logout" onClick={logout}>
+          Logout
+        </button>
+      </div>
+      <div className="settings-information">
+        <article>
+          <h3>Name</h3>
+          <p>{user.name}</p>
+        </article>
+        <article>
+          <h3>Email</h3>
+          <p>{user.email}</p>
+        </article>
+      </div>
+    </div>
+  );
 };
 
 export default Settings;
